43b358ebc697cdf6e4fc2838a5229e7fd7b760de
Even70000 <75841107+Even70000@users.noreply.github.com>
2021-04-09 13:49:14 +0200

    rdmr

---
+++ admin/includes/footer.php
@@ -0,0 +1,35 @@
+<div class="footer">
+  <div class="contain">
+    <div class="dummytext">
+        Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw.
+    </div>
+    <!-- <div class="footerlogo">
+      <img src="./img/logo.png" alt="Logo" height="160px" width="260px">
+    </div> -->
+    <div class="dummytext">
+    <a>info@vistacars.nl</a>
+    <br>
+      <a>+31 (0)6 00 11 55 11</a>    
+    </div>
+  </div>
+</div>
+
+
+
+
+<script src="assets/js/main.js"></script>
+
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+  <script>
+    AOS.init();
+  </script>
+
+<script type="text/javascript">
+    window.addEventListener("scroll", function(){
+        var header = document.querySelector("header");
+        header.classList.toggle("sticky", window.scrollY > 0)
+    })
+</script>
+
+</body>
+</html>
