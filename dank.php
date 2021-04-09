941bcfaa2a7f9d85040d85e376e57e365d67a718
Even70000 <75841107+Even70000@users.noreply.github.com>
2021-04-09 11:00:16 +0200

    dank word gedaan

---
+++ dank.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/6488e6347e.js" crossorigin="anonymous"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css"/>
+  <script src="https://code.jquery.com/jquery-latest.min.js"></script>
+  <link rel="stylesheet" href="css/dank.css"/>
+    <title>buurtzorg</title>
+</head>
+<body>
+  <div id="dankWord">
+      Bedankt voor uw sollicitatie!
+      We zullen er zo snel mogelijk
+      naar kijken.
+  </div>
+</body>
+</html>
